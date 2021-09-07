c47c04871ca6dc0bcf76246088f86194641ed890
Mark Alston <malston@vmware.com>
2021-09-07 08:15:17 -0600

    Remove extra whitepace

--- scripts/init-cluster.sh
+++ scripts/init-cluster.sh
@@ -11,9 +11,9 @@ set -o errexit -o nounset -o pipefail
 
 function main() {
   readParameters "$@"
-  
+
   [[ $TRACE == true ]] && set -x;
-  
+
   # Install k3d if necessary
   if ! command -v k3d >/dev/null 2>&1; then
     installK3d
@@ -63,7 +63,7 @@ function createCluster() {
       '--network=host'
     )
   else
-    # Internal Docker registry must be on localhost. Otherwise docker will use HTTPS, leading to errors on docker push 
+    # Internal Docker registry must be on localhost. Otherwise docker will use HTTPS, leading to errors on docker push
     # in the example application's Jenkins Jobs.
     # If available, use default port for playground registry, because no parameter is required when applying
     if command -v netstat >/dev/null 2>&1 && ! netstat -an | grep 30000 | grep LISTEN >/dev/null 2>&1; then
@@ -108,7 +108,7 @@ function confirm() {
   # shellcheck disable=SC2145
   # - the line break between args is intended here!
   printf "%s\n" "${@:-Are you sure? [y/N]} "
-  
+
   read -r response
   case "$response" in
   [yY][eE][sS] | [yY])
@@ -125,9 +125,9 @@ readParameters() {
     -o hx \
     --long help,cluster-name:,bind-localhost:,trace \
     -- "$@")
-  
+
   eval set -- "$COMMANDS"
-  
+
   CLUSTER_NAME=gitops-playground
   BIND_LOCALHOST=true
   TRACE=false
